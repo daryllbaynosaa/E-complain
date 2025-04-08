5d195bfe055c382fb0a05de7812a30868a5706eb
enpeasant007 <daryllbaynosa8@gmail.com>
2025-04-08 23:09:25 +0800

    feat: updated Applayout and views

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -8,14 +8,18 @@ function onClick() {
 }
 </script>
 <template>
-  <v-responsive class="border rounded" max-height="300">
+  <v-responsive class="border rounded">
     <v-app :theme="theme">
-      <v-app-bar class="px-3">
+      <v-app-bar
+        class="px-3"
+        :color="theme === 'light' ? 'grey-lighten-1' : 'grey-darken-3'"
+        border
+      >
         <v-spacer></v-spacer>
 
         <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
+          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          variant="elevated"
           slim
           @click="onClick"
         ></v-btn>
@@ -23,9 +27,30 @@ function onClick() {
 
       <v-main>
         <v-container>
-          <h1>Main Content</h1>
+          <v-row class="d-flex justify-center">
+            <v-col cols="12" md="6">
+              <v-card class="mx-auto" prepend-icon="">
+                <v-card-title class="text-center">
+                  <span class="font-weight-black">E-Complain</span>
+                  <P>login form</P>
+                </v-card-title>
+
+                <v-card-text class="bg-surface-light pt-4">
+                  <v-divider class="my-5"></v-divider>
+                  <v-form fast-fail @submit.prevent>
+                    <v-text-field label="email" variant="outlined"></v-text-field>
+
+                    <v-text-field label="password" variant="outlined"></v-text-field>
+
+                    <v-btn class="mt-2" type="submit" prepend-icon="mdi-login" block> login </v-btn>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-container>
       </v-main>
+      <v-footer border app>@2025 Barangay Complain System</v-footer>
     </v-app>
   </v-responsive>
 </template>
