9756322cf9836e40115abd6e4ce20e9c35b00494
enpeasant007 <daryllbaynosa8@gmail.com>
2025-04-29 16:02:03 +0800

    fix: updated .env

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/auth/LoginView.vue'
 import SignView from '../views/auth/SignView.vue'
 import LoginPage2 from '../views/auth/LoginPage2.vue'
-
+import PostFeed from '../views/auth/PostFeed.vue'
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', name: 'Login', component: LoginView },
   { path: '/signup', name: 'Signup', component: SignView },
-  { path: '/login2', name: 'LoginPage2', component: LoginPage2 },
+  { path: '/postfeed', name: 'PostFeed', component: PostFeed },
+  { path: '/login2', name: 'Login2', component: LoginPage2 },
 ]
 
 const router = createRouter({
